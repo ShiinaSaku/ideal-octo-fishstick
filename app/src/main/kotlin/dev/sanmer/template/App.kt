b9c303f872fd712e091bd760c5b4ead37ec7a2a6
サクシイナ <saksham.mac@icloud.com>
2025-12-18 21:50:42 +0530

    U

--- app/src/main/kotlin/dev/sanmer/template/App.kt
+++ app/src/main/kotlin/dev/sanmer/template/App.kt
@@ -1,19 +1,18 @@
 package dev.sanmer.template
 
-import android.app.Application
-import dev.sanmer.template.di.Repositories
-import dev.sanmer.template.di.ViewModels
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.GlobalContext.startKoin
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import dev.sanmer.template.navigation.AppNavGraph
+import dev.sanmer.template.ui.theme.ShinnaTheme
 
-class App : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        startKoin {
-            androidLogger()
-            androidContext(this@App)
-            modules(Repositories, ViewModels)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShinnaTheme {
+                AppNavGraph()
+            }
         }
     }
 }
