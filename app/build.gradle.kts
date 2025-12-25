2b4cdff01b00c41e3040374af2dd494c38d830f2
サクシイナ <saksham.mac@icloud.com>
2025-12-25 23:12:43 +0530

    D

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material3:material3-window-size-class")
-    implementation("androidx.compose.material3:material3-adaptive")
+    implementation("androidx.compose.material3.adaptive:adaptive:1.3.0-alpha05")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation("com.squareup.retrofit2:converter-gson:3.0.0")
